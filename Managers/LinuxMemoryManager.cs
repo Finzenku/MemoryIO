e61c62988efe3d202168e1d45b6af261b189a901
Katherine Foley <kevfol@hotmail.com>
2023-06-04 11:24:52 -1000

    LinuxMemoryManager libc read/write Implemented libc's process_vm_readv and process_vm_writev methods that were introduced in Linux 3.2 Allowed unsafe code to run in the project. Removed dependancy on 3rd party libSimpleLinuxMemoryAccess.so Reordered and rewrote all IMemoryManger methods for the new implementation. Fixed a bug in is64BitProcess check that was only reading 4 bytes but needed the 5th.

--- Managers/LinuxMemoryManager.cs
+++ Managers/LinuxMemoryManager.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Text;
-using MemoryManagement.Internals;
 
 namespace MemoryManagement.Managers
 {
@@ -13,16 +11,13 @@ namespace MemoryManagement.Managers
         private bool is64BitProcess;
         public bool Is64BitProcess => is64BitProcess;
 
-        [DllImport(@"Memory/libSimpleLinuxMemoryAccess.so")]
-        public static extern IntPtr ReadMemory(int pid, IntPtr address, int maxLenght);
+
+        [DllImport("libc")]
+        private static extern unsafe int process_vm_readv(int pid, iovec* local_iov, ulong liovcnt, iovec* remote_iov, ulong riovcnt, ulong flags);
+        [DllImport("libc")]
+        private static extern unsafe int process_vm_writev(int pid, iovec* local_iov, ulong iiovcnt, iovec* remote_iov, ulong riovcnt, ulong flags);
         
-        [DllImport(@"Memory/libSimpleLinuxMemoryAccess.so")]
-        public static extern void WriteMemory(int pid, IntPtr address, byte[] value, int valueSize);
-        
-        [DllImport(@"Memory/libSimpleLinuxMemoryAccess.so")]
-        public static extern IntPtr freeingMemory(IntPtr address);
-        
-        [DllImport ("libc")]
+        [DllImport("libc")]
         public static extern uint getuid ();
         
         public LinuxMemoryManager(Process process)
@@ -37,10 +32,10 @@ namespace MemoryManagement.Managers
 
                 using (var stream = new FileStream(processPath, FileMode.Open, FileAccess.Read))
                 {
-                    byte[] buffer = new byte[4];
+                    byte[] buffer = new byte[5];
                     if (stream.Read(buffer, 0, buffer.Length) == buffer.Length)
                     {
-                        // The first 4 bytes of the ELF header indicate the architecture
+                        // The 5th byte of the ELF header indicate the architecture
                         // 0x01: 32-bit, 0x02: 64-bit
                         is64BitProcess = buffer[4] == 0x02;
                     }
@@ -53,33 +48,95 @@ namespace MemoryManagement.Managers
             }
         }
 
+        #region Read
+        private unsafe bool Read(IntPtr address, void* dataPointer, int dataLength)
+        {
+            var localIo = new iovec
+            {
+                iov_base = dataPointer,
+                iov_len = dataLength
+            };
+
+            var remoteIo = new iovec
+            {
+                iov_base = address.ToPointer(),
+                iov_len = dataLength
+            };
+
+            return process_vm_readv(Process.Id, &localIo, 1, &remoteIo, 1, 0) != -1;
+        }
+
+        public byte[] ReadData(IntPtr address, int dataLength)
+        {
+            byte[] dataBuffer = new byte[dataLength];
+            unsafe
+            {
+                fixed (byte* bufferPtr = dataBuffer)
+                {
+                    Read(address, bufferPtr, dataLength);
+                }
+            }
+            return dataBuffer;
+        }
+
+        public T Read<T>(IntPtr address)
+        {
+            T result = default!;
+            int tSize = Marshal.SizeOf<T>();
+            unsafe
+            {
+                IntPtr pDataPointer = Marshal.AllocHGlobal(tSize);
+                if (Read(address, pDataPointer.ToPointer(), tSize))
+                {
+                    result = Marshal.PtrToStructure<T>(pDataPointer)!;
+                }
+            }
+            return result;
+        }
+        public T[] ReadArray<T>(IntPtr address, int arrayLength)
+        {
+            int tSize = Marshal.SizeOf<T>();
+            int dataLength = tSize * arrayLength;
+            T[] array = new T[arrayLength];
+
+            IntPtr pDataPointer = Marshal.AllocHGlobal(dataLength);
+            try
+            {
+                unsafe
+                {
+                    if (Read(address, pDataPointer.ToPointer(), dataLength))
+                    {
+                        for (int i = 0; i < arrayLength; i++)
+                        {
+                            IntPtr tAddress = IntPtr.Add(pDataPointer, i * tSize);
+                            array[i] = Marshal.PtrToStructure<T>(tAddress)!;
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(pDataPointer);
+            }
+
+            return array;
+        }
+
         public string ReadString(IntPtr address, Encoding encoding, int maxLength = 512)
         {
-            
-            IntPtr value = ReadMemory(Process.Id, address,maxLength);
-            byte[] byteValue = new byte[maxLength];
-            Marshal.Copy(value, byteValue, 0, maxLength);
+            byte[] byteValue = ReadData(address, maxLength);
             string encoded = encoding.GetString(byteValue);
-
-            freeingMemory(value);
 
             // Search the end of the string
             var endOfStringPosition = encoded.IndexOf('\0');
 
             // Crop the string with this end if found, return the string otherwise
-            return endOfStringPosition == -1 ? encoded : encoded.Substring(0, endOfStringPosition);
-            //return encoded;
-            
+            return endOfStringPosition == -1 ? encoded : encoded.Substring(0, endOfStringPosition);            
         }
-
         public string[] ReadStringArray(IntPtr address, Encoding encoding, int maxLength = 512)
         {
-            IntPtr value = ReadMemory(Process.Id, address, maxLength);
-            byte[] byteValue = new byte[maxLength];
-            Marshal.Copy(value, byteValue, 0, maxLength);
+            byte[] byteValue = ReadData(address, maxLength);
             string encoded = encoding.GetString(byteValue);
-
-            freeingMemory(value);
 
             string[] split = encoded.Split('\0');
             for (int i = 0; i < split.Length; i++)
@@ -87,64 +144,129 @@ namespace MemoryManagement.Managers
                     return split[0..i];
             return split;
         }
+        #endregion
 
-        public T Read<T>(IntPtr address)
+        #region Write
+        private unsafe bool Write(IntPtr address, void* dataPointer, int dataLength)
         {
-            IntPtr value = ReadMemory(Process.Id, address,MarshalType<T>.Size);           
-            T temp = Marshal.PtrToStructure<T>(value)!;
+            var localIo = new iovec
+            {
+                iov_base = dataPointer,
+                iov_len = dataLength
+            };
 
-            freeingMemory(value);
-            return temp;
+            var remoteIo = new iovec
+            {
+                iov_base = address.ToPointer(),
+                iov_len = dataLength
+            };
+
+            return process_vm_writev(Process.Id, &localIo, 1, &remoteIo, 1, 0) != -1;
         }
-
-        public byte[] ReadData(IntPtr address, int dataLength)
+        public void Write<T>(IntPtr address, T value)
         {
-            IntPtr value = ReadMemory(Process.Id, address, dataLength);
-            byte[] dataBuffer = new byte[dataLength];
-            Marshal.Copy(value, dataBuffer, 0, dataBuffer.Length);
-
-            freeingMemory(value);
-            return dataBuffer;
+            if (value is null)
+                return;
+            int tSize = Marshal.SizeOf<T>();
+            IntPtr dataPointer = Marshal.AllocHGlobal(tSize);
+            try
+            {
+                Marshal.StructureToPtr(value, dataPointer, false);
+                unsafe
+                {
+                    Write(address, dataPointer.ToPointer(), tSize);
+                }
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(dataPointer);
+            }
         }
-
-        //Basic implementation, leave for Zackmon someday :)
-        public T[] ReadArray<T>(IntPtr address, int arrayLength)
+        public void WriteArray<T>(IntPtr address, T[] value)
         {
-            T[] array = new T[arrayLength];
-            for (int i = 0; i < array.Length; i++)
-                array[i] = Read<T>(address + i * MarshalType<T>.Size);
-            return array;
+            if (value is null || value.Length == 0)
+                return;
+
+            int dataLength = Marshal.SizeOf<T>() * value.Length;
+
+            if (value is byte[] byteArray)
+            {
+                unsafe
+                {
+                    fixed (byte* valuePtr = byteArray)
+                    {
+                        Write(address, valuePtr, dataLength);
+                    }
+                }
+                return;
+            }
+
+            IntPtr dataPointer = Marshal.AllocHGlobal(dataLength);
+            try
+            {
+                byte[] buffer = new byte[dataLength];
+                Buffer.BlockCopy(value, 0, buffer, 0, dataLength);
+
+                unsafe
+                {
+                    fixed (byte* bufferPtr = buffer)
+                    {
+                        Write(address, bufferPtr, dataLength);
+                    }
+                }
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(dataPointer);
+            }
         }
 
         public void WriteString(IntPtr address, string text, Encoding encoding)
         {
-            byte[] value = encoding.GetBytes(text+ '\0');
-            WriteMemory(Process.Id,address,value,value.Length);
+            byte[] buffer = encoding.GetBytes(text+'\0');
+            unsafe
+            {
+                fixed (byte* bufferPtr = buffer)
+                {
+                    Write(address, bufferPtr, buffer.Length);
+                }
+            }
         }
         public void WriteStringArray(IntPtr address, string[] text, Encoding encoding)
         {
-            List<byte> value = new();
+            int totalLength = 0;
             foreach (string s in text)
-                value.AddRange(encoding.GetBytes(s+'\0'));
-            WriteMemory(Process.Id, address, value.ToArray(), value.Count);
-        }
+                totalLength += encoding.GetByteCount(s) + 1; // +1 for null termination
 
-        public void Write<T>(IntPtr address, T value)
-        {
-            byte[] byteValue = MarshalType<T>.ObjectToByteArray(value);
-            WriteMemory(Process.Id,address,byteValue,byteValue.Length);
-        }
-        //Basic implementation, leave for Zackmon someday :)
-        public void WriteArray<T>(IntPtr address, T[] value)
-        {
-            for (int i = 0; i < value.Length; i++)
-                Write(address + i * MarshalType<T>.Size, value[i]);
-        }
+            byte[] buffer = new byte[totalLength];
+            int offset = 0;
 
+            foreach (string s in text)
+            {
+                int byteCount = encoding.GetBytes(s, buffer.AsSpan(offset));
+                buffer[offset + byteCount] = 0; // Null termination
+                offset += byteCount + 1; // Move the offset to the next string
+            }
 
-        public static uint checkPrivileges()
+            unsafe
+            {
+                fixed (byte* bufferPtr = buffer)
+                {
+                    Write(address, bufferPtr, buffer.Length);
+                }
+            }
+        }
+        #endregion
+
+        public static uint CheckPrivileges()
         {
             return getuid();
+        }
+
+        private unsafe struct iovec
+        {
+            public void* iov_base;
+            public int iov_len;
         }
     }
 }
